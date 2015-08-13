ea7835526bcb4fe20d9b9e845df6b540b9fbc6bd
Ednara Oliveira <ednaraoliveira@gmail.com>
2015-08-13 08:50:41 -0300

    Testes

--- SECertificate/src/main/java/simple/example/linux/CertificateSignerLinux.java
+++ SECertificate/src/main/java/simple/example/linux/CertificateSignerLinux.java
@@ -2,6 +2,7 @@ package simple.example.linux;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,19 +11,21 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
+import java.security.Provider;
+import java.security.Security;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import br.gov.frameworkdemoiselle.certificate.keystore.loader.implementation.FileSystemKeyStoreLoader;
 import br.gov.frameworkdemoiselle.certificate.signer.SignerException;
 import br.gov.frameworkdemoiselle.certificate.signer.factory.PKCS7Factory;
 import br.gov.frameworkdemoiselle.certificate.signer.pkcs7.PKCS7Signer;
-import br.gov.frameworkdemoiselle.certificate.signer.pkcs7.bc.policies.ADRBCMS_2_1;
+import br.gov.frameworkdemoiselle.policy.engine.factory.PolicyFactory.Policies;
 
 
 public class CertificateSignerLinux {
@@ -33,57 +36,49 @@ public class CertificateSignerLinux {
 	public static void main(String[] args) throws KeyStoreException {
 
 		String configName = "/home/01534562567/drivers.config";
-		String PIN = "homolo";
+		String PIN = "qwaszx12!";
 		Certificate[] certificates = null;
+		byte[] content = null;
 
-		/* Obtendo a chave privada */
 
 		String alias;
 		try {
 			logger.info("-------- Fabrica do certificate --------");
-			
-			File file2 = new File("/home/01534562567/LEONARDO_ALEXANDRE_FERREIRA_LEITE.p12");
-			KeyStore keyStore = (new FileSystemKeyStoreLoader(file2)).getKeyStore(PIN);
+
+			Provider p = new sun.security.pkcs11.SunPKCS11(configName);
+			Security.addProvider(p);
+
+			KeyStore keyStore = KeyStore.getInstance("PKCS11", "SunPKCS11-Provedor");
+			keyStore.load(null, PIN.toCharArray());
 
 			alias = (String) keyStore.aliases().nextElement();
+			logger.info("alias ...........: {}", alias);
+			PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias,
+					PIN.toCharArray());
+			logger.info("privateKey ......: {}", privateKey);
 
-			X509Certificate certificate = (X509Certificate) keyStore.getCertificate(alias);
-			alias = (String) keyStore.aliases().nextElement();
-			PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias,	PIN.toCharArray());
-			
+			X509Certificate c = (X509Certificate) keyStore
+					.getCertificate(alias);
 
-//			Provider p = new sun.security.pkcs11.SunPKCS11(configName);
-//			Security.addProvider(p);
-//
-//			KeyStore keyStore = KeyStore.getInstance("PKCS11", "SunPKCS11-Provedor");
-//			keyStore.load(null, PIN.toCharArray());
-//
-//			alias = (String) keyStore.aliases().nextElement();
-//			logger.info("alias ...........: {}", alias);
-//			PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias,
-//					PIN.toCharArray());
-//			logger.info("privateKey ......: {}", privateKey);
-//
-//			X509Certificate c = (X509Certificate) keyStore
-//					.getCertificate(alias);
+			FileInputStream inputStream2 = new FileInputStream("motor.jpg");
+			try {
+				content = IOUtils.toByteArray(inputStream2);
+			} finally {
+				inputStream2.close();
+			}
 
-			byte[] content = "SERPRO".getBytes();
 
-			/* Parametrizando o objeto doSign */
 			PKCS7Signer signer = PKCS7Factory.getInstance().factoryDefault();
 			signer.setCertificates(keyStore.getCertificateChain(alias));
-			//signer.setPrivateKey((PrivateKey) keyStore.getKey(alias, null));
 			signer.setPrivateKey(privateKey);
-			//signer.setSignaturePolicy(Policies.AD_RB_CADES_2_1);
-			signer.setSignaturePolicy(new ADRBCMS_2_1());
+			signer.setSignaturePolicy(Policies.AD_RB_CADES_2_1);
+			//signer.setSignaturePolicy(new ADRBCMS_2_1());
 			signer.setAttached(true);
 
-			/* Realiza a assinatura do conteudo */
 			logger.info("Efetuando a  assinatura do conteudo");
-			//byte[] signed = signer.doSign(content);
-			byte[] signed = signer.signer(content);
+			byte[] signed = signer.doSign(content);
+			//byte[] signed = signer.signer(content);
 
-			/* Valida o conteudo */
 			logger.info("Efetuando a validacao da assinatura.");
 			boolean checked = signer.check(content, signed);
 
@@ -93,9 +88,8 @@ public class CertificateSignerLinux {
 				logger.info("A assinatura foi invalidada!");
 			}
 
-			/* Exportando a assintatura */
 			logger.info("Exportando a assintatura.");
-			File file = new File("assinaturaV21RB.p7s"); 
+			File file = new File("assinaturaV201RB.p7s"); 
 			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file)); // Criamos o arquivo
 			bos.write(signed); // Gravamos os bytes lá
 			bos.close(); // Fechamos o stream.
@@ -119,6 +113,12 @@ public class CertificateSignerLinux {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 		} catch (SignerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CertificateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
